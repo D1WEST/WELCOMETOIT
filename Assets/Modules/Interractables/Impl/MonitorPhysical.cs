d8b37d8ea37856f1c677060f6c5c69c9f34c364a
DIWEST <freddyprosha@gmail.com>
2026-04-26 00:05:54 +0300

    Monitor with mount and logic

--- Assets/Modules/Interractables/Impl/MonitorPhysical.cs
+++ Assets/Modules/Interractables/Impl/MonitorPhysical.cs
@@ -1,69 +1,99 @@
-﻿using Assets.Modules.Interractables;
-using Assets.Modules.Interractables.Impl;
+﻿using System.Collections.Generic;
 using Unity.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
-public class MonitorPhysical : MonoBehaviour, IInteractable
+namespace Assets.Modules.Interractables.Impl
 {
-    [ReadOnly] public string targetWorkplaceId; // Помечаем как ReadOnly (если есть такой атрибут) или просто прячем
-    public WorkplaceInteractable sourceDesk;
-
-    private Rigidbody _rb;
-    private bool _isCarried = false;
-
-    // Свойства интерфейса
-    public string InteractionPrompt => _isCarried ? "" : $"Поднять монитор ({targetWorkplaceId}) [E]";
-    public Transform InteractionPivot => transform;
-    public InteractionType InteractionType => InteractionType.Click;
-    public float HoldDuration => 0;
-
-    private void Awake()
+    [RequireComponent(typeof(Rigidbody))]
+    public class MonitorPhysical : MonoBehaviour, IInteractable
     {
-        _rb = GetComponent<Rigidbody>();
-        SetPhysics(false);
-    }
+        [ReadOnly] public string targetWorkplaceId;
+        public WorkplaceInteractable sourceDesk;
 
-    public void Initialize(string id, WorkplaceInteractable desk)
-    {
-        targetWorkplaceId = id;
-        sourceDesk = desk;
-        SetPhysics(false);
-    }
+        [Header("Visuals")]
+        [SerializeField] private Material materialOn;  // Весь монитор "горит" (или обычный вид)
+        [SerializeField] private Material materialOff; // Весь монитор "погас" (темный)
+        private List<MeshRenderer> _allRenderers = new List<MeshRenderer>();
 
-    public void Interact(GameObject interactor)
-    {
-        // Если в руках уже что-то есть - игнорируем
-        if (PlayerInteraction.Instance.IsCarryingItem) return;
+        private Rigidbody _rb;
+        private bool _isCarried = false;
 
-        if (sourceDesk != null)
+        public string InteractionPrompt => _isCarried ? "" : $"Поднять монитор ({targetWorkplaceId}) [E]";
+        public Transform InteractionPivot => transform;
+        public InteractionType InteractionType => InteractionType.Click;
+        public float HoldDuration => 0;
+
+        private void Awake()
         {
-            sourceDesk.OnMonitorManualPickUp();
-            sourceDesk = null;
-        }
-        PlayerInteraction.Instance.PickUpMonitor(this);
-    }
+            _rb = GetComponent<Rigidbody>();
 
-    public void SetPhysics(bool state)
-    {
-        if (_rb == null) _rb = GetComponent<Rigidbody>();
-        _rb.isKinematic = !state;
-        _rb.useGravity = state;
-        if (state)
+            // Находим все части монитора, которые имеют меш
+            _allRenderers.AddRange(GetComponentsInChildren<MeshRenderer>());
+
+            SetPhysics(false);
+            SetVisualState(false); // Изначально выключен
+        }
+
+        public void Initialize(string id, WorkplaceInteractable desk)
         {
-            if (TryGetComponent<Collider>(out var col)) col.enabled = true;
+            targetWorkplaceId = id;
+            sourceDesk = desk;
+            SetPhysics(false);
+            SetVisualState(true); // Включаем при установке на стол
         }
-    }
 
-    public void GetKicked()
-    {
-        transform.SetParent(null);
-        SetPhysics(true);
+        public void Interact(GameObject interactor)
+        {
+            if (PlayerInteraction.Instance.IsCarryingItem) return;
 
-        Vector3 kickDir = (transform.forward + Vector3.up + Random.insideUnitSphere * 0.5f).normalized;
-        float kickPower = 12f;
+            if (sourceDesk != null)
+            {
+                sourceDesk.OnMonitorManualPickUp();
+                sourceDesk = null;
+            }
 
-        _rb.AddForce(kickDir * kickPower, ForceMode.Impulse);
-        _rb.AddTorque(Random.onUnitSphere * 10f, ForceMode.Impulse);
+            SetVisualState(false); // Гасим при подборе
+            PlayerInteraction.Instance.PickUpMonitor(this);
+        }
+
+        public void SetPhysics(bool state)
+        {
+            if (_rb == null) _rb = GetComponent<Rigidbody>();
+            _rb.isKinematic = !state;
+            _rb.useGravity = state;
+            if (state)
+            {
+                if (TryGetComponent<Collider>(out var col)) col.enabled = true;
+            }
+        }
+
+        public void GetKicked()
+        {
+            transform.SetParent(null);
+            SetPhysics(true);
+            SetVisualState(false); // Гасим при пинке
+
+            Vector3 kickDir = (transform.forward + Vector3.up + Random.insideUnitSphere * 0.5f).normalized;
+            float kickPower = 12f;
+
+            _rb.AddForce(kickDir * kickPower, ForceMode.Impulse);
+            _rb.AddTorque(Random.onUnitSphere * 10f, ForceMode.Impulse);
+        }
+
+        // МЕТОД ДЛЯ СМЕНЫ МАТЕРИАЛА ВСЕГО ОБЪЕКТА
+        public void SetVisualState(bool isOn)
+        {
+            Material targetMat = isOn ? materialOn : materialOff;
+
+            if (targetMat == null) return;
+
+            foreach (var rend in _allRenderers)
+            {
+                if (rend != null)
+                {
+                    rend.material = targetMat;
+                }
+            }
+        }
     }
 }
